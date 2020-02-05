6de8fd6af33cfbddc2025b7e6cb1b58d102d6eee
waramsey <46686986+waramsey@users.noreply.github.com>
2020-02-05 13:34:52 -0600

    Sprint 1 - Beginning of Class
    Pushing the Player class for Ryan to work on.

---
+++ GameEngine/Player.js
@@ -0,0 +1,14 @@
+import GameObject from "./GameObject";
+
+class Player extends GameObject {
+
+    //will probably need components for:
+        //character model
+        //user input (move & swing sword)
+        //collision
+    constructor() {
+
+    }
+}
+
+export default Player;
