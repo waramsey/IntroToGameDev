0a2c2e5469d332b5f327d680bd98a8e40da91a1b
waramsey <46686986+waramsey@users.noreply.github.com>
2020-02-12 18:01:20 -0600

    Major Updates to Display
    The game now starts on the start screen, beautifully centered in minimalist style.  It includes an elegant transition into the game scene, now bordered and centered as well.

--- GameEngine/TextComponent.js
+++ GameEngine/TextComponent.js
@@ -17,7 +17,6 @@ class TextComponent extends Component{
             ctx.fillStyle = this.fill;
             ctx.font = this.font;
             ctx.fillText(this.text,-ctx.measureText(this.text).width/2,0);
-
         }
         ctx.restore();
     }
--- GameEngine/game.js
+++ GameEngine/game.js
@@ -4,62 +4,47 @@ import TileComponent from "./TileComponent.js"
 import PlayerComponent from "./PlayerComponent.js"
 import TextComponent from "./TextComponent.js"
 import PlayerBehavior from "./PlayerBehavior.js"
-import TextBehavior from "./TextBehavior.js";
-import EmitParticles from "./EmitParticles.js";
+import TextBehavior from "./TextBehavior.js"
+import EmitParticles from "./EmitParticles.js"
 import GrowShrink from "./GrowShrink.js"
 
 let gameScene = new Scene();
-let startScene = new Scene();
-
-//START SCENE
-{
-    let title = new GameObject(320,100);
-    startScene.gameObjects.push(title);
-    title.addComponent(new TextComponent("Main Menu","small-caps 50px georgia","white"));
-
-    //button to change scenes?
-    let button = new GameObject(320,300);
-    startScene.gameObjects.push(button);
-    button.addComponent(new PlayerComponent(200,150,"white","white"))
-
-}
-
-//GAME SCENE
-{
-    // PlayerGameObject.addComponent(new EmitParticles("black",6,gameScene));
-
-    let tile = new GameObject(200,200)
-    gameScene.gameObjects.push(tile)
-    tile.addComponent(new TileComponent("black","white"))
-    tile.addComponent(new EmitParticles("black",6,gameScene))
-
-    let PlayerGameObject = new GameObject(100,100);
-    gameScene.gameObjects.push(PlayerGameObject);
-    let playerComponent = new PlayerComponent(50, 50, "white", "white");
-    let playerBehavior = new PlayerBehavior();
-    PlayerGameObject.addComponent(playerComponent);
-    PlayerGameObject.addComponent(playerBehavior);
-    // PlayerGameObject.addComponent(new GrowShrink());
-
-    let textTimer = new GameObject(555,50);
-    gameScene.gameObjects.push(textTimer);
-    let textComponent  = new TextComponent(200,"small-caps 20px georgia","white");
-    let textBehavior = new TextBehavior();
-    textTimer.addComponent(textComponent);
-    textTimer.addComponent(textBehavior);
-}
-
-
-let currentScene = startScene;
-
+let currentScene = gameScene;
+let screen = document.getElementById('screen');
 let canv, ctx;
+    
 
-function main() {
+document.getElementById('button').onclick = function startGame() {
+    screen.innerHTML = "<canvas id='canv' width='640px' height='480px'></canvas>"
     canv = document.querySelector("#canv");
     ctx = canv.getContext('2d');
-
     setInterval(gameLoop, 33);
+    currentScene = gameScene;
+
+    //GAME SCENE
+    {
+        let tile = new GameObject(200,200)
+        gameScene.gameObjects.push(tile)
+        tile.addComponent(new TileComponent("black","white"))
+        tile.addComponent(new EmitParticles("black",6,gameScene))
+
+        let PlayerGameObject = new GameObject(100,100);
+        gameScene.gameObjects.push(PlayerGameObject);
+        let playerComponent = new PlayerComponent(50, 50, "white", "white");
+        let playerBehavior = new PlayerBehavior();
+        PlayerGameObject.addComponent(playerComponent);
+        PlayerGameObject.addComponent(playerBehavior);
+        // PlayerGameObject.addComponent(new GrowShrink());
+
+        let textTimer = new GameObject(555,50);
+        gameScene.gameObjects.push(textTimer);
+        let textComponent  = new TextComponent(200,"small-caps 20px georgia","white");
+        let textBehavior = new TextBehavior();
+        textTimer.addComponent(textComponent);
+        textTimer.addComponent(textBehavior);
+    }
 }
+
 
 function gameLoop() {
     update();
@@ -73,5 +58,3 @@ function update() {
 function draw(ctx) {
     currentScene.draw(ctx,canv.width,canv.height);
 }
-
-main();
