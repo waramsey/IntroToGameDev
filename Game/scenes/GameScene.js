3ce8f0eb09bf3ccfd3b7b266e463baaa37c53ccc
waramsey <46686986+waramsey@users.noreply.github.com>
2020-03-05 10:36:16 -0600

    Made a Base Room and a TestRoom prefab
    Much like how we can switch scenes by placing a new scene on the game, we should be able to switch rooms by placing a new room on the scene and removing the old one.  This update includes the beginnings of a framework to do this.

--- Game/scenes/GameScene.js
+++ Game/scenes/GameScene.js
@@ -8,17 +8,8 @@ export default class GameScene extends Engine.Base.Scene {
     constructor() {
         super("GameScene");
 
-        let CrystalGameObject = new GameObjects.Crystal(500,200);
-        this.children.push(CrystalGameObject);
-
-        let tile = new GameObject(200,200)
-        this.children.push(tile)
-        tile.addComponent(new Components.TileComponent("black","white"))
-
-        let BossGameObject = new GameObject(300,100);
-        this.children.push(BossGameObject);
-        BossGameObject.addComponent(new Components.RectangleComponent(100, 100, "red", "white"));
-        BossGameObject.addComponent(new GameBehaviors.BossBehavior());
+        let TestRoom = new GameObjects.TestRoom();
+        this.children.push(TestRoom)
 
         let PlayerGameObject = new GameObject(100,100);
         this.children.push(PlayerGameObject);
@@ -29,5 +20,7 @@ export default class GameScene extends Engine.Base.Scene {
         this.children.push(textTimer);
         textTimer.addComponent(new Components.TextComponent(200,"small-caps 20px georgia","white"));
         textTimer.addComponent(new GameBehaviors.TextBehavior());
+
+
     }
 }
