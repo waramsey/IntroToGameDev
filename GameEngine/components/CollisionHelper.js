cc93992f9e9bd8bec53a605052525743a07bea71
Narducky <47222373+Narducky@users.noreply.github.com>
2020-03-10 14:11:43 -0500

    Update CollisionHelper.js

--- GameEngine/components/CollisionHelper.js
+++ GameEngine/components/CollisionHelper.js
@@ -5,18 +5,6 @@ import PointCollider from "./PointCollider.js";
 export default class CollisionHelper{
    
         static inCollision(one, two){
-            if(one.collider instanceof CircleCollider && two.collider instanceof RectangleCollider){
-                let distance = one.gameObject.location.distance(two.gameObject.location);
-                
-                if(distance < one.collider.radius)
-                    return true;
-                return false;
-            }
-            else if(one.collider instanceof RectangleCollider && two.collider instanceof CircleCollider){
-                return this.inCollision(two,one);
-
-            }
-            
             if(one.collider instanceof CircleCollider && two.collider instanceof PointCollider){
                 let distance = one.gameObject.location.distance(two.gameObject.location);
                 
@@ -24,10 +12,27 @@ export default class CollisionHelper{
                     return true;
                 return false;
             }
+            else if(one.collider instanceof RectangleCollider && two.collider instanceof PointCollider){
+                //
+            }
+            else if(one.collider instanceof CircleCollider && two.collider instanceof RectangleCollider){
+                //
+            }
+            else if(one.collider instanceof CircleCollider && two.collider instanceof CircleCollider){
+                //
+            }
+            else if(one.collider instanceof RectangleCollider && two.collider instanceof RectangleCollider){
+                //
+            }   
             else if(one.collider instanceof PointCollider && two.collider instanceof CircleCollider){
                 return this.inCollision(two,one);
             }
-            
+            else if(one.collider instanceof PointCollider && two.collider instanceof RectangleCollider){
+                return this.inCollision(two,one);
+            }
+            else if(one.collider instanceof RectangleCollider && two.collider instanceof CircleCollider){
+                return this.inCollision(two,one);
+            }            
         }
     
 }
