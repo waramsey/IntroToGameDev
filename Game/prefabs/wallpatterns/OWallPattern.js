08510bbd4880943f0c5c612ab26b5277ca7ce6fc
waramsey <waramsey@unomaha.edu>
2020-05-01 18:13:07 -0500

    2 More Rooms To Go

---
+++ Game/prefabs/wallpatterns/OWallPattern.js
@@ -0,0 +1,46 @@
+export default{
+    name: "OWallPattern",
+    children: [
+        {
+            name: "w1",
+            location: {x:275,y:175},
+            type: "Wall"
+        },
+        {
+            name: "w2",
+            location: {x:275,y:225},
+            type: "Wall"
+        },
+        {
+            name: "w3",
+            location: {x:275,y:275},
+            type: "Wall"
+        },
+        {
+            name: "w4",
+            location: {x:325,y:175},
+            type: "Wall"
+        },
+        {
+            name: "w5",
+            location: {x:325,y:275},
+            type: "Wall"
+        },
+        {
+            name: "w6",
+            location: {x:375,y:175},
+            type: "Wall"
+        },
+        {
+            name: "w7",
+            location: {x:375,y:225},
+            type: "Wall"
+        },
+        {
+            name: "w8",
+            location: {x:375,y:275},
+            type: "Wall"
+        },
+        
+    ]
+}
--- Game/scenes/GameScene.js
+++ Game/scenes/GameScene.js
@@ -56,9 +56,14 @@ export default {
             type: "RBL" //Change this to any combination of TRBL (in that order) to change walls
         },
             {
-                name: "RoundedCornerPattern",
+                name: "CupNorthPattern",
                 location: {x:-650,y:-900},
-                type: "RoundedCornerPattern"
+                type: "CupNorthPattern"
+            },
+            {
+                name: "FourCornersPattern",
+                location: {x:-650,y:-900},
+                type: "FourCornersPattern"
             },
         {
             name: "0/2",
@@ -115,6 +120,11 @@ export default {
             location: {x:650,y:-450},
             type: "TL" //Change this to any combination of TRBL (in that order) to change walls
         },
+            {
+                name: "RoundedCornerPattern",
+                location: {x:650,y:-450},
+                type: "RoundedCornerPattern"
+            },
         {
             name: "2/0",
             location: {x:-1300,y:0},
@@ -166,6 +176,16 @@ export default {
             location: {x:650,y:0},
             type: "RBL" //Change this to any combination of TRBL (in that order) to change walls
         },
+            {
+                name: "FourCornersPattern",
+                location: {x:650,y:0},
+                type: "FourCornersPattern"
+            },
+            {
+                name: "PlusWallPattern",
+                location: {x:650,y:0},
+                type: "PlusWallPattern"
+            },
         {
             name: "2/4",
             location: {x:1300,y:0},
@@ -186,6 +206,16 @@ export default {
             location: {x:-1300,y:450},
             type: "TR" //Change this to any combination of TRBL (in that order) to change walls
         },
+            {
+                name: "OWallPattern",
+                location: {x:-1300,y:450},
+                type: "OWallPattern"
+            },
+            {
+                name: "FourInnerWallPattern",
+                location: {x:-1300,y:450},
+                type: "FourInnerWallPattern"
+            },
         {
             name: "3/1",
             location: {x:-650,y:450},
@@ -201,11 +231,31 @@ export default {
             location: {x:0,y:450},
             type: "TRL" //Change this to any combination of TRBL (in that order) to change walls
         },
+            {
+                name: "FourInnerWallPattern",
+                location: {x:0,y:450},
+                type: "FourInnerWallPattern"
+            },
+            {
+                name: "FourCornersPattern",
+                location: {x:0,y:450},
+                type: "FourCornersPattern"
+            },
         {
             name: "3/3",
             location: {x:650,y:450},
             type: "TL" //Change this to any combination of TRBL (in that order) to change walls
         },
+            {
+                name: "OWallPattern",
+                location: {x:650,y:450},
+                type: "OWallPattern"
+            },
+            {
+                name: "RoundedCornerPattern",
+                location: {x:650,y:450},
+                type: "RoundedCornerPattern"
+            },
         
 
         //PLAYER IS LAST
