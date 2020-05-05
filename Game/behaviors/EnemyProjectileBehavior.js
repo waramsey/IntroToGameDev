6c93611fce281edc60dda5fa94e14b8f26a71706
waramsey <waramsey@unomaha.edu>
2020-05-04 20:09:20 -0500

    Fixed projectile collision issue

--- Game/behaviors/EnemyProjectileBehavior.js
+++ Game/behaviors/EnemyProjectileBehavior.js
@@ -20,7 +20,7 @@ export default class EnemyProjectileBehavior
         if (collisionObject.gameObject.tag == "Player")
             collisionObject.gameObject.components.filter(i => i.takeDamage).forEach(i => i.takeDamage(3));
 
-        if (collisionObject.gameObject.tag != "Camera" && collisionObject.gameObject.tag != "pickup" && collisionObject.gameObject.tag != "Projectile" && collisionObject.gameObject.tag != "Boss")
+        if (collisionObject.gameObject.tag != "Camera" && collisionObject.gameObject.tag != "pickup" && collisionObject.gameObject.tag != "projectile" && collisionObject.gameObject.tag != "Boss")
             this.gameObject.delete = true;
         // if (collisionObject.gameObject.tag != "Camera" && collisionObject.gameObject.tag != "pickup")
         //     this.gameObject.delete = true;
